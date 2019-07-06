6394d766f2290e52b533deb0adbf9b889266cd4b
Yehor Melnyk <egorchikmelnyk@gmail.com>
2019-07-05 17:42:44 -0700

    Added Delete Stream Component, Edit Stream Component with functionality, also made rtmp server for live videos with using OBS

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -24,7 +24,8 @@ class StreamList extends React.Component{
             return (
                 <div className="item" key={stream.id}>
                     <i className="large middle aligned icon camera"/>
-                    <div className="content">{stream.title}
+                    <div className="content">
+                        <Link to={`/streams/${stream.id}`} className="header">{stream.title}</Link>
                         <div className="description">{stream.description}</div>
                     </div>
                     {this.renderAdmin(stream)}
